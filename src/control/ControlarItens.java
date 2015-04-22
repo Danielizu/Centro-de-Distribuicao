6af02fdf1b406beedf5b0f7f17697997f068789d
Danielizu <danielizu3@hotmail.com>
2015-04-22 01:18:01 -0300

    Update ControlarItens.java

--- src/control/ControlarItens.java
+++ src/control/ControlarItens.java
@@ -2,12 +2,10 @@ package control;
 
 import boundary.FrmConsultaNF;
 import entity.Item;
-
 import java.io.*;
-
 import javax.swing.JComboBox;
 import javax.swing.JTextField;
-
+import com.toedter.calendar.JDateChooser;
 
 public class ControlarItens {
 
@@ -17,6 +15,9 @@ public class ControlarItens {
 	
 	public void LimparCampos(JTextField texto) {
 		texto.setText(null);
-		
+	}
+	public void LimparData (JDateChooser texto){
+		texto.setToolTipText(null);
+	
 	}
 }
