adffb014ee8c2f6e3bdcfbf38065cafecebd46b9
Danielizu <danielizu3@hotmail.com>
2015-04-23 02:31:55 -0300

    Update Item.java

--- src/entity/Item.java
+++ src/entity/Item.java
@@ -2,4 +2,42 @@ package entity;
 
 public class Item {
 
+	private String cliente;
+	private String nota;
+	private String data;
+	
+	public Item (){
+		this ("", "", "");
+	}
+	public Item (String cliente, String nota, String data){
+		this.cliente = cliente;
+		this.nota = nota;
+		this.data = data;
+		
+	}
+	public String getCliente() {
+		return cliente;
+	}
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
+	public String getNota() {
+		return nota;
+	}
+	public void setNota(String nota) {
+		this.nota = nota;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
